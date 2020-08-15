82063865e4ceb4dabb0b6b14606c52e041def02f
Rahavee <rmanian@uvm.edu>
2020-08-14 21:25:35 -0400

    made NASDAQ data local

--- stocks.py
+++ stocks.py
@@ -2,18 +2,20 @@ import pandas_datareader
 import re
 import datetime
 
-result = pandas_datareader.nasdaq_trader.get_nasdaq_symbols(retry_count=3, timeout=30, pause=None)
+file = open("stock.txt", "r")
+result = []
+for i in range(0, 17996):
+    result.append(re.search(".*", file.readline()).group())
 
-print(len(result))
+file.close()
+
 
 
 def autoComplete(search):
     match = []
-
-    for i in range(0, len(result)):
-        if re.search(str(search), result.iloc[i, 9]) is not None:
-            print(result.iloc[i, 9])
-            match.append({"Name": result.iloc[i, 1], "Symbol": result.iloc[i, 9]})
+    for i in range(1, len(result),2):
+        if re.search(str(search), result[i]) is not None:
+            match.append({"Name": result[i-1], "Symbol": result[i]})
         if len(match) == 11:
             break
 
@@ -22,8 +24,9 @@ def autoComplete(search):
 
 def getStockData(stock):
     data = pandas_datareader.DataReader(stock, data_source="yahoo", start="2013-01-01", end=datetime.date.today())
-    closingPrice=[]
+    closingPrice = []
     for i in range(0, len(data)):
-        closingPrice.append(float(data.iloc[i,3]))
+        closingPrice.append(float(data.iloc[i, 3]))
     return closingPrice
 
+print(autoComplete("AAP"))
