c546e02062446dbde1dbcd656d17b16aa1453ae7
Rahavee <rmanian@uvm.edu>
2020-08-19 12:40:07 -0400

    return type is {x:date, y:price}

--- stocks.py
+++ stocks.py
@@ -10,12 +10,11 @@ for i in range(0, 17996):
 file.close()
 
 
-
 def autoComplete(search):
     match = []
-    for i in range(1, len(result),2):
+    for i in range(1, len(result), 2):
         if re.search(str(search), result[i]) is not None:
-            match.append({"Name": result[i-1], "Symbol": result[i]})
+            match.append({"Name": result[i - 1], "Symbol": result[i]})
         if len(match) == 11:
             break
 
@@ -26,6 +25,5 @@ def getStockData(stock):
     data = pandas_datareader.DataReader(stock, data_source="yahoo", start="2017-01-01", end=datetime.date.today())
     closingPrice = []
     for i in range(0, len(data)):
-        closingPrice.append({"date":data.iloc[i,0], "price" :float(data.iloc[i, 3])})
+        closingPrice.append({"x": data.iloc[i, 0], "y": float(data.iloc[i, 3])})
     return closingPrice
-
